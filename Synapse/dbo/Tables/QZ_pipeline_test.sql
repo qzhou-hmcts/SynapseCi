9f9882efd64568d69b84544f0d3aa2eefa491a45
Qiang Zhou <Qiang.Zhou@HMCTS.NET>
2020-09-15 21:36:22 +0100

    Add changes

--- Synapse/dbo/Tables/QZ_pipeline_test.sql
+++ Synapse/dbo/Tables/QZ_pipeline_test.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[QZ_pipeline_test] (
-    [col1] INT NULL,
-    [col2] INT NULL
+    [col1] INT          NULL,
+    [col2] INT          NULL,
+    [col3] VARCHAR (20) NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
+
 
