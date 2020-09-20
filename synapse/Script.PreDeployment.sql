2e6467e0b9dc9e9ee0914b13f70cac3a7a17b029
qzhou <qiang.zhou@hmcts.net>
2020-09-20 01:27:54 +0100

    Removed pre deployment script

--- synapse/Script.PreDeployment.sql
+++ synapse/Script.PreDeployment.sql
@@ -2,9 +2,9 @@
 Post-Deployment Script Template							
 --------------------------------------------------------------------------------------
  This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
  Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
  Example:      :setvar TableName MyTable							
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
@@ -12,19 +12,3 @@ Post-Deployment Script Template
 
 :setvar PipelinePassword PipelinePassword
 :setvar SqlDwDatabaseName SqlDwDatabaseName
-
-use master
-go
-IF NOT EXISTS
-    (
-        SELECT *
-        FROM sys.sql_logins
-        WHERE name = 'pipeline'
-    )
-
-    BEGIN
-        CREATE LOGIN pipeline WITH PASSWORD = '$(PipelinePassword)'
-    END
-
-use $(SqlDwDatabaseName)
-go
