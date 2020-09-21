ceb1f7a8fce78065404c0fbb85699cb7226a23f3
qzhou <qiang.zhou@hmcts.net>
2020-09-21 23:09:45 +0100

    Fix environment variables

--- src/integrationTest/conftest.py
+++ src/integrationTest/conftest.py
@@ -13,20 +13,20 @@ load_dotenv(verbose=True)
 @pytest.fixture(scope="session")
 def config():
     return {
-        "AZ_SQL_SERVER_NAME": os.getenv("AZ_SQL_SERVER_NAME"),
-        "AZ_SQL_SERVER_USERNAME": os.getenv("AZ_SQL_SERVER_USERNAME"),
-        "AZ_SQL_SERVER_PASSWORD": os.getenv("AZ_SQL_SERVER_PASSWORD"),
-        "AZ_SQL_DATABASE_NAME": os.getenv("AZ_SQL_DATABASE_NAME"),
+        "SQL_SERVER_NAME": os.getenv("SQL_SERVER_NAME"),
+        "SQL_SERVER_USERNAME": os.getenv("SQL_SERVER_USERNAME"),
+        "SQL_SERVER_PASSWORD": os.getenv("SQL_SERVER_PASSWORD"),
+        "SQL_DATABASE_NAME": os.getenv("SQL_DATABASE_NAME"),
     }
 
 
 @pytest.fixture(scope="module")
 def sql_connection(config):
     """Create a Connection Object object"""
-    server = config["AZ_SQL_SERVER_NAME"]
-    database = config["AZ_SQL_DATABASE_NAME"]
-    username = config["AZ_SQL_SERVER_USERNAME"]
-    password = config["AZ_SQL_SERVER_PASSWORD"]
+    server = config["SQL_SERVER_NAME"]
+    database = config["SQL_DATABASE_NAME"]
+    username = config["SQL_SERVER_USERNAME"]
+    password = config["SQL_SERVER_PASSWORD"]
 
     connection = """Driver={{ODBC Driver 17 for SQL Server}};Server=tcp:{dbServer}.database.windows.net,1433; \
     Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'"""\
