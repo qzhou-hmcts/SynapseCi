ad1c08578d28bd5c326cc84657413c56875836a9
qzhou <qiang.zhou@hmcts.net>
2020-09-23 10:47:09 +0100

    update adf test

--- src/integrationTest/test_pipeline_ingestion.py
+++ src/integrationTest/test_pipeline_ingestion.py
@@ -1,14 +1,18 @@
-PIPELINE_NAME = ""
+PIPELINE_NAME = "copy_pipeline"
 
 
-def test_pipeline_succeeded(sql_connection):
+def test_pipeline_succeeded(adf_pipeline_run):
     """Test that pipeline has data in SQL"""
-    # this_run = adf_pipeline_run(PIPELINE_NAME, run_inputs={})
+    this_run = adf_pipeline_run(PIPELINE_NAME, run_inputs={})
 
-    # Assert
-    cursor = sql_connection.cursor()
-    cursor.execute("SELECT COUNT(*)  as 'rowCount' from PROBATE.STG_CCD_METADATA")
-    row = cursor.fetchone()
-    # assert this_run.status == "Succeeded"
-    assert row is not None
-    assert int(row.rowCount) >= 1
+    assert this_run.status == "Succeeded"
+
+# def test_db_connection() {
+#     cursor = sql_connection.cursor()
+#     cursor.execute("SELECT COUNT(*)  as 'rowCount' from PROBATE.STG_CCD_METADATA")
+#     row = cursor.fetchone()
+
+#     assert row is not None
+#     assert int(row.rowCount) >= 1
+# }
+
