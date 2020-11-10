a904d14237eb3ad7f1d5ea004a8d8a0279aaaf6c
qzhou <qiang.zhou@hmcts.net>
2020-11-10 00:17:54 +0000

    Update generic deployment

--- _scripts/deploySqlPackage.sh
+++ _scripts/deploySqlPackage.sh
@@ -2,4 +2,33 @@
 
 DYLD_PRINT_LIBRARIES=1
 
-sqlpackage /a:$DeploymentAction /su:$SqlUsername /ssn:$ServerName /sdn:$DatabaseName  /sp:$SqlPassword /tf:backup.dacpac
+arguments=''
+function getProperty() {
+    if [ -z "$2" ]
+    then
+        echo ""
+    else
+        echo "$1$2" 
+    fi
+}  
+
+arguments="$arguments $(getProperty /a: $DeploymentAction)"
+arguments="$arguments $(getProperty /su: $SqlUsername)"
+arguments="$arguments $(getProperty /ssn: $ServerName)"
+arguments="$arguments $(getProperty /sdn: $DatabaseName)"
+arguments="$arguments $(getProperty /sp: $SqlPassword)"
+arguments="$arguments $(getProperty /tf: $TargetFile)"
+arguments="$arguments $(getProperty /sf: $DacpacFile)"
+arguments="$arguments $(getProperty /tdn: $TargetDatabaseName)"
+arguments="$arguments $(getProperty /tsn: $TargetServerName)"
+arguments="$arguments $(getProperty /tp: $TargetPassword)"
+arguments="$arguments $(getProperty /tu: $TargetUser)"
+
+if [ -z "$arguments" ]
+then
+    echo "empty arguments"
+else
+    arguments="$arguments $AdditionalArguments"
+fi
+
+sqlpackage $arguments
