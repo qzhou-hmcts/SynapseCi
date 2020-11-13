db0208660ae44bbf5cd1b3211ef4af56e0ed750c
qzhou <qiang.zhou@hmcts.net>
2020-11-13 21:45:33 +0000

    add parentheses

--- _scripts/deploySqlPackage.ps1
+++ _scripts/deploySqlPackage.ps1
@@ -5,7 +5,7 @@ $Command='/Users/qzhou/test/sqlpackage-osx-x64/sqlpackage'
 $numOfArgs = $args.Length - 1
 for ($i=0; $i -le $numOfArgs; $i++)
 {
-   $Command="$Command $($args[$i])"
+   $Command="$Command '$($args[$i])'"
 }
 
 Write-Host  "============================"
