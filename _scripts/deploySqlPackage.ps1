fdb23aaf4409a888ba5f982c30db08da1d1818e8
qzhou <qiang.zhou@hmcts.net>
2020-11-13 18:54:17 +0000

    test PS script

---
+++ _scripts/deploySqlPackage.ps1
@@ -0,0 +1,14 @@
+
+
+
+$Command='/Users/qzhou/test/sqlpackage-osx-x64/sqlpackage'
+$numOfArgs = $args.Length - 1
+for ($i=0; $i -le $numOfArgs; $i++)
+{
+   $Command="$Command $($args[$i])"
+}
+
+Write-Host  "============================"
+
+Write-Host  $Command
+$Command | Invoke-Expression
