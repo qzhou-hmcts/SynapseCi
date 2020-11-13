33df28bead526afe938c4ac691e6f8b485932bc4
qzhou <qiang.zhou@hmcts.net>
2020-11-13 22:55:53 +0000

    Remove action

--- _scripts/deploySqlPackage.ps1
+++ _scripts/deploySqlPackage.ps1
@@ -5,8 +5,10 @@ $Command='sqlpackage'
 $numOfArgs = $args.Length - 1
 for ($i=0; $i -le $numOfArgs; $i++)
 {
-   $Command="$Command '$($args[$i])'"
+   $Command="$Command $($args[$i])"
 }
+
+Write-Host  "============================"
 
 Write-Host  $Command
 $Command | Invoke-Expression
