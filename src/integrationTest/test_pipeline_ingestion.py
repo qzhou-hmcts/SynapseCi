c02cdab58dea0696034f6f27c9d4a45ba0355c39
qzhou <qiang.zhou@hmcts.net>
2020-11-23 21:50:27 +0000

    enable sql and publish artifact

--- src/integrationTest/test_pipeline_ingestion.py
+++ src/integrationTest/test_pipeline_ingestion.py
@@ -7,12 +7,12 @@ def test_pipeline_succeeded(adf_pipeline_run):
 
     assert this_run.status == "Succeeded"
 
-# def test_db_connection() {
-#     cursor = sql_connection.cursor()
-#     cursor.execute("SELECT COUNT(*)  as 'rowCount' from PROBATE.STG_CCD_METADATA")
-#     row = cursor.fetchone()
+def test_db_connection(sql_connection) {
+    cursor = sql_connection.cursor()
+    cursor.execute("SELECT COUNT(*)  as 'rowCount' from payment.BulkEnvelopCase")
+    row = cursor.fetchone()
 
-#     assert row is not None
-#     assert int(row.rowCount) >= 1
-# }
+    assert row is not None
+    assert int(row.rowCount) >= 1
+}
 
